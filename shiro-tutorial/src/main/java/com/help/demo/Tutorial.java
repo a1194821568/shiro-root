7fc28a20da5b98846f7ed6029eb7aeaca297c2a1
a11948215568 <1194821568@qq.com>
2019-05-29 16:35:34 +0800

    添加注释

--- shiro-tutorial/src/main/java/com/help/demo/Tutorial.java
+++ shiro-tutorial/src/main/java/com/help/demo/Tutorial.java
@@ -60,10 +60,38 @@ public class Tutorial {
 	        	UsernamePasswordToken token = new UsernamePasswordToken("lonestarr", "vespa");
 
 	            //支持'remember me' (无需配置，内建的!):
-	            token.setRememberMe(true);
+	        	/*Shiro 支持在登录过程中执行"remember me"，在此值得指出，一个已记住的 
+	        	 * Subject（remembered Subject）和一个正常通过认证的 Subject
+	        	 * （authenticated Subject）在 Shiro 是完全不同的。
+				 *
+				 *记住的（Remembered）：一个被记住的 Subject 不不会是匿名的，拥有一个已知的身份
+				 *也就是说subject.getPrincipals())返回非空）。它的身份被先前的认证过程所记住，
+				 *并存于先前session中，一个被认为记住的对象在执行subject.isRemembered())返回true。
+				 *
+				 *已验证（Authenticated）：一个被验证的 Subject 是成功验证后（如登录成功）
+				 *并存于当前 session 中，一个被认为验证过的对象调用subject.isAuthenticated()) 
+				 *将返回true。
+				 *
+				 *
+				 *已记住（Remembered）和已验证（Authenticated）是互斥的--一个标识值为真另一个就为假，反过来也一样。
+	        	 */
+	        	 //下面这个API上的例子有助于理解下面这个rememberMe的作用
+	        	/**
+	        	 *  下面是一个非常常见的场景帮助说明被记住和被验证之间差别为何重要。
+				 *	假设你使用Amazon.com，你已经成功登录并且在购物篮中添加了一些书籍，但你由于临时要参加一个会议，匆忙中你忘记退出登录，当会议结束，回家的时间到了，于是你离开了办公室。
+				 *	第二天当你回到工作，你意识到你没有完成你的购买动作，于是你回到Amazon.com，这时，Amazon.com“记得”是是，通过你的名字向你打招呼，仍旧给你提供个性化的图书推荐，对于Amazon.com，subject.isRemembered() 将返回真。
+				 *	但是当你想访问你帐号的信用卡信息完成图书购买的时候会怎样呢？虽然Amazon.com “记住”了你（isRemembered() == true），它不能担保你就是你（也许是正在使用你计算机的同事）。
+				 *	于是，在你执行像使用信用卡信息之类的敏感操作之前，Amazon.com 强制你登录以使他们担保你的身份，在你登录之后，你的身份已经被验证，对于Amazon.com，isAuthenticated()将返回真。
+				 *	这类情景经常发生，所以 Shiro 加入了该功能，你可以在你的程序中使用。现在是使用 isRemembered() 还是使用 isAuthenticated() 来定制你的视图和工作流完全取决于你自己，但 Shiro 维护这种状态基础以防你可能会需要。
+	             */
+	             token.setRememberMe(true);
 	            //
 	           
+	        //Shiro拥有丰富的运行期异常AuthenticationException可以精确标明为何验证失败，
+	        //你可以将 login 放入到 try/catch 块中并捕获所有你想捕获的异常并对它们做出处理    
 	        try{
+	        	//当身份和证明住处被收集并实例化为一个 AuthenticationToken （认证令牌）后，
+	        	//我们需要向 Shiro 提交令牌以执行真正的验证尝试：
 	            currentUser.login(token);
 	            //无异常，说明就是我们想要的!
 	            System.out.println("//无异常，说明就是我们想要的!");
@@ -110,7 +138,15 @@ public class Tutorial {
 	        }
 
 	        //完成 - 退出t!
-	        currentUser.logout();//清楚识别信息，设置 session 失效.
+	        /*Web 程序需注意
+	         * 
+  			 *因为在 Web 程序中记住身份信息往往使用 Cookies，
+  			 *而 Cookies 只能在 Response 提交时才能被删除，
+  			 *所以强烈要求在为最终用户调用subject.logout() 之后立即将用户引导到一个新页面，
+  			 *确保任何与安全相关的 Cookies 如期删除，
+  			 *这是 Http 本身 Cookies 功能的限制而不是 Shiro 的限制。
+  			 */
+	        currentUser.logout();//清除验证信息，使 session 失效
 	        
 	        System.exit(0);
 	}
